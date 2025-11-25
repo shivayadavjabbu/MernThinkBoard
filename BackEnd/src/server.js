ede8f924391cb7441e078afb8f328712c26297b1
Shivayadav <shivayadavjabbu@gmail.com>
2025-11-26 01:03:05 +0530

    Adds MongoDB connection utility
    Introduces an async function to handle MongoDB connection with error handling and status logging, enabling centralized database setup for the backend.

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoutes.js';
+import connectDB from 'config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
  
