727f2eb656fbcb276b65c7115fe310a40f714eb3
Shivayadav <shivayadavjabbu@gmail.com>
2025-11-26 00:03:57 +0530

    Adds basic notes API endpoint
    Introduces a GET endpoint for notes to enable backend
    response testing and integration with frontend requests.
    Facilitates initial API connectivity.

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,11 @@
 const express = require('express');
 const app = express()
 
+app.get('/api/notes', (req, res) => {
+    //got the request and now sending responding message
+    res.status(200).send('Hello from the backend!'); 
+});
+
 app.listen(5001, () => {
     console.log('Server is running on port 5001')
 }); 
