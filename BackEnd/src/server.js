8b81d8e9182f611965fcac4ed2f42cb537190aff
Shivayadav <shivayadavjabbu@gmail.com>
2025-11-26 11:22:35 +0530

    Improves server config and startup logic
    Switches to environment-based port selection for flexibility.
    Adds JSON body parsing to support API requests.
    Corrects database config import path and logs MongoDB URI for easier debugging.

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,18 +1,21 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoutes.js';
-import connectDB from 'config/db.js';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express()
+app.use(express.json());
+
 const PORT = process.env.PORT || 5001;
 
+// Connect to MongoDB
 connectDB();
 
 app.use("/api/notes", notesRoutes);
  
-app.listen(5001, () => {
-    console.log('Server is running on port 5001')
+app.listen(PORT, () => {
+    console.log('Server is running on port :' + PORT);
 }); 
 
