05f1413ce695bf10183070dbb8491afd3b278cac
Shivayadav <shivayadavjabbu@gmail.com>
2025-12-01 19:43:13 +0530

    Improvement : Added middleware
    The upStash is used as a middleware.
    We modified how serve starts. The problem is we are running the server before the
    Db connection is successfull. So to avoid  that we create connection to the DB then using promises
    we start a server.

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,20 +2,35 @@ import express from 'express'
 import notesRoutes from './routes/notesRoutes.js';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
+import ratelimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express()
-app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
 // Connect to MongoDB
-connectDB();
+
+
+app.use(express.json()); // this is a middle ware that gives access to the body
+
+app.use(ratelimiter); 
+
+//our simple custom middle ware 
+app.use((req, res, next) => {
+    console.log(`Req method is ${req.method} and url is ${req.url}`); 
+    next(); 
+}); 
 
 app.use("/api/notes", notesRoutes);
- 
-app.listen(PORT, () => {
-    console.log('Server is running on port :' + PORT);
+
+//CONNECT THE DATABASE THEN RUN THE SERVER
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port :' + PORT);
+    }); 
 }); 
+
+
 
